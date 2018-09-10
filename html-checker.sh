eb850bcd7b1626f16ac77414b9e342a52aa4e17c
Scott Cline <scott@rs21.io>
2018-09-10 07:47:49 -0600

    Update checker code.

--- html-checker.sh
+++ html-checker.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-exit 1
+tarball=$1
+
+if [ "$tarball" == "" ] || [ ! -s $tarball ]; then
+  echo "Unable to find tarball artifact."
+  exit 1
+fi
+
+mkdir source
+cd source
+tar fxvz ../$tarball
+ls -l
+exit 0
